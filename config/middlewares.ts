172b5f4b00ebcea54ee385d7f2bcf97d6b5cf643
Andres Proaño <pmja2209@gmail.com>
2024-11-30 05:17:09 -0500

    Update middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,31 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https:"],
+          "media-src": ["'self'", "data:", "blob:", "https:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  {
+    name: "strapi::session",
+    config: {
+      secure: true, // Asegura que las cookies solo se envíen a través de HTTPS
+      sameSite: "none", // Compatible con navegadores modernos
+    },
+  },
+  "strapi::favicon",
+  "strapi::public",
 ];
