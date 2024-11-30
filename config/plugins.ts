c46e050597fa19a7ec00ffea86d9ac9e6a447d6f
Andres Proaño <pmja2209@gmail.com>
2024-11-30 05:23:02 -0500

    Fix middlewares

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,18 +1,1 @@
-export default ({ env }) => ({
-  "users-permissions": {
-    config: {
-      providers: {
-        google: {
-          clientId: env("GOOGLE_CLIENT_ID"),
-          clientSecret: env("GOOGLE_CLIENT_SECRET"),
-          redirectUri: `${env("BASE_URL")}/api/connect/google/callback`,
-        },
-        facebook: {
-          clientId: env("FACEBOOK_CLIENT_ID"),
-          clientSecret: env("FACEBOOK_CLIENT_SECRET"),
-          redirectUri: `${env("BASE_URL")}/api/connect/facebook/callback`,
-        },
-      },
-    },
-  },
-});
+export default () => ({});
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("BASE_URL", "https://finder-api-production.up.railway.app"),
   app: {
     keys: env.array("APP_KEYS"),
   },
