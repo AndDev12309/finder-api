78996a91f0735a1dd5fd5dba97add8b7d7ac7661
Andres Proaño <pmja2209@gmail.com>
2024-11-30 05:50:00 -0500

    Fix 3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,32 +1,12 @@
 export default [
-  "strapi::logger",
-  "strapi::errors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::cors",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        "http://localhost:3000", // Tu frontend local
-        "https://finder-rosy.vercel.app", // Tu frontend en producción
-      ],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      credentials: true, // Permitir cookies
-    },
-  },
-  "strapi::security",
-  "strapi::session",
-  {
-    name: "strapi::session",
-    config: {
-      cookie: {
-        secure: true, // Permitir solo en HTTPS
-        sameSite: "none", // Necesario para aplicaciones cruzadas (frontend-backend en dominios distintos)
-      },
-    },
-  },
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
