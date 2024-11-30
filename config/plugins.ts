43a587ae2153d83eebb7c19048e8737d36705d82
Andres Proaño <pmja2209@gmail.com>
2024-11-30 04:29:49 -0500

    Add auth with google

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({ env }) => ({
+  "users-permissions": {
+    config: {
+      providers: {
+        google: {
+          clientId: env("GOOGLE_CLIENT_ID"),
+          clientSecret: env("GOOGLE_CLIENT_SECRET"),
+          redirectUri: env("STRAPI_URL") + "/api/auth/callback/google",
+        },
+      },
+    },
+  },
+});
