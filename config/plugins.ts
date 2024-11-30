0b6db5cc723c3e0d27543b327ad09379c6dd7713
Andres Proaño <pmja2209@gmail.com>
2024-11-30 04:45:06 -0500

    Update providers auth

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,12 +5,12 @@ export default ({ env }) => ({
         google: {
           clientId: env("GOOGLE_CLIENT_ID"),
           clientSecret: env("GOOGLE_CLIENT_SECRET"),
-          redirectUri: `${env("BASE_URL")}/api/auth/callback/google`,
+          redirectUri: `${env("BASE_URL")}/api/connect/google/callback`,
         },
         facebook: {
           clientId: env("FACEBOOK_CLIENT_ID"),
           clientSecret: env("FACEBOOK_CLIENT_SECRET"),
-          redirectUri: `${env("BASE_URL")}/api/auth/callback/facebook`,
+          redirectUri: `${env("BASE_URL")}/api/connect/facebook/callback`,
         },
       },
     },
