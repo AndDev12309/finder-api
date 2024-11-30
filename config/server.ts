ce09a75fc192a7991fd5eb1a95544eaf0bcc0e2f
Andres Proaño <pmja2209@gmail.com>
2024-11-30 06:54:37 -0500

    Fix final

--- config/server.ts
+++ config/server.ts
@@ -1,9 +1,7 @@
 export default ({ env }) => ({
-  proxy: true,
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env("BASE_URL", "https://finder-api-production.up.railway.app"),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array('APP_KEYS'),
   },
 });
