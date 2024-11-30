0a746cd8eaa2aa842406ed5b520d92719e546b1f
Andres Proaño <pmja2209@gmail.com>
2024-11-30 04:34:36 -0500

    Add auth with google-facebook

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,7 +5,12 @@ export default ({ env }) => ({
         google: {
           clientId: env("GOOGLE_CLIENT_ID"),
           clientSecret: env("GOOGLE_CLIENT_SECRET"),
-          redirectUri: env("STRAPI_URL") + "/api/auth/callback/google",
+          redirectUri: `${env("BASE_URL")}/api/auth/callback/google`,
+        },
+        facebook: {
+          clientId: env("FACEBOOK_CLIENT_ID"),
+          clientSecret: env("FACEBOOK_CLIENT_SECRET"),
+          redirectUri: `${env("BASE_URL")}/api/auth/callback/facebook`,
         },
       },
     },
